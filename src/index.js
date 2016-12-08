8ea9651e70b5354213da2b9e3b7ec8a2f26318e0
aostrovsky <anatolyostrovsky@gmail.com>
2016-12-08 14:14:17 +0200

    Add linter

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ export const nullable = (boolean) => {
 };
 
 export const blank = (boolean) => {
-  const isString = x => nonEmpty(x) && typeof x.valueOf() == 'string';
+  const isString = x => nonEmpty(x) && typeof x.valueOf() === 'string';
   if (boolean === false) return x => isString(x) && x.length > 0;
   else return x => isString(x);
 };
