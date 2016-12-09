d05c0690fb48e30579853be92ced86c0e210ff3c
aostrovsky <anatolyostrovsky@gmail.com>
2016-12-09 14:29:22 +0200

    Refactor to array spec inputs

--- spec/blank.spec.js
+++ spec/blank.spec.js
@@ -2,37 +2,12 @@ import { blank } from '../src/index';
 
 describe('blank constaint', () => {
   describe('unsuccessful validation with blank set to false', () => {
-    it('should be invalid', () => {
-      expect(blank(false)()).toBe(false);
-    });
 
-    it('should be invalid', () => {
-      expect(blank(false)(null)).toBe(false);
-    });
-
-    it('should be invalid', () => {
-      expect(blank(false)(undefined)).toBe(false);
-    });
-
-    it('should be invalid', () => {
-      expect(blank(false)(1)).toBe(false);
-    });
-
-    it('should be invalid', () => {
-      expect(blank(false)({})).toBe(false);
-    });
-
-    it('should be invalid', () => {
-      expect(blank(false)(new Object())).toBe(false);
-    });
-
-    it('should be invalid', () => {
-      expect(blank(false)("")).toBe(false);
-    });
-
-    it('should be invalid', () => {
-      expect(blank(false)(new String(""))).toBe(false);
-    });
+    [, null, undefined, 1, {}, new Object(), "", new String("")].forEach(x =>
+      it('should be invalid', () => {
+        expect(blank(false)(x)).toBe(false);
+      })
+    );
   });
 
   describe('successful validation with blank set to false', () => {
@@ -42,32 +17,20 @@ describe('blank constaint', () => {
   });
 
   describe('unsuccessful validation with blank set to true', () => {
-    it('should be invalid', () => {
-      expect(blank(true)()).toBe(false);
-    });
-
-    it('should be invalid', () => {
-      expect(blank(true)(null)).toBe(false);
-    });
-
-    it('should be invalid', () => {
-      expect(blank(true)(undefined)).toBe(false);
-    });
-
-    it('should be invalid', () => {
-      expect(blank(true)(1)).toBe(false);
-    });
+    [, null, undefined, 1].forEach(x =>
+      it('should be invalid', () => {
+        expect(blank(true)(x)).toBe(false);
+      })
+    );
 
   });
 
   describe('successful validation with blank set to true', () => {
-    it('should be valid', () => {
-      expect(blank(true)("")).toBe(true);
-    });
-
-    it('should be valid', () => {
-      expect(blank(true)(new String(""))).toBe(true);
-    });
+    ["", new String("")].forEach(x =>
+      it('should be valid', () => {
+        expect(blank(true)(x)).toBe(true);
+      })
+    );
   });
 
 });
--- spec/nullable.spec.js
+++ spec/nullable.spec.js
@@ -2,49 +2,27 @@ import { nullable } from '../src/index';
 
 describe('nullable constaint', () => {
   describe('unsuccessful validation with nullable set to false', () => {
-    it('should be invalid', () => {
-      expect(nullable(false)()).toBe(false);
-    });
-
-    it('should be invalid', () => {
-      expect(nullable(false)(null)).toBe(false);
-    });
-
-    it('should be invalid', () => {
-      expect(nullable(false)(undefined)).toBe(false);
-    });
+    [, null, undefined].forEach(x =>
+      it('should be invalid', () => {
+        expect(nullable(false)(x)).toBe(false);
+      })
+    );
   });
 
   describe('successful validation with nullable set to false', () => {
-    it('should be valid', () => {
-      expect(nullable(false)(1)).toBe(true);
-    });
-
-    it('should be valid', () => {
-      expect(nullable(false)("")).toBe(true);
-    });
-
-    it('should be valid', () => {
-      expect(nullable(false)({})).toBe(true);
-    });
-
-    it('should be valid', () => {
-      expect(nullable(false)(0)).toBe(true);
-    });
+    [1, "", {}, 0].forEach(x =>
+      it('should be valid', () => {
+        expect(nullable(false)(x)).toBe(true);
+      })
+    );
   });
 
   describe('successful validation with nullable set to true', () => {
-    it('should be valid', () => {
-      expect(nullable(true)()).toBe(true);
-    });
-
-    it('should be valid', () => {
-      expect(nullable(true)(null)).toBe(true);
-    });
-
-    it('should be valid', () => {
-      expect(nullable(true)(undefined)).toBe(true);
-    });
+    [, null, undefined].forEach(x =>
+      it('should be valid', () => {
+        expect(nullable(true)(x)).toBe(true);
+      })
+    );
   });
 
 });
