a36d6522cd5a6dea60ccae010e542f150d918c81
tolyo <anatolyostrovsky@gmail.com>
2016-12-17 00:31:34 +0200

    format

--- spec/size.spec.js
+++ spec/size.spec.js
@@ -3,22 +3,22 @@ import test from 'tape';
 
 test('size constrain validation with size set to invalid values', (assert) => {
 
-    [{}, 1, null, undefined, false, true].forEach( x =>
-      assert.throws(() => size(x), 'size constraint requires String parameter')
-    );
+  [{}, 1, null, undefined, false, true].forEach( x =>
+    assert.throws(() => size(x), 'size constraint requires String parameter')
+  );
 
-    ['asdf', '3313', '1<1'].forEach( x =>
-        assert.throws(() => size(x), 'size constraint requires parameter to conform to Groovy range')
-    );
+  ['asdf', '3313', '1<1'].forEach( x =>
+    assert.throws(() => size(x), 'size constraint requires parameter to conform to Groovy range')
+  );
 
-    ['7..6', '7..7', '99<..<6'].forEach( x =>
-        assert.throws(() => size(x), 'size constraint requires min to be less than max')
-    );
+  ['7..6', '7..7', '99<..<6'].forEach( x =>
+    assert.throws(() => size(x), 'size constraint requires min to be less than max')
+  );
 
-    ['asdf', '3313', '1<1'].forEach( x =>
-      assert.throws(() => size(x), 'size constraint requires parameter to conform to Groovy range')
-    );
+  ['asdf', '3313', '1<1'].forEach( x =>
+    assert.throws(() => size(x), 'size constraint requires parameter to conform to Groovy range')
+  );
 
-    assert.end();
+  assert.end();
 });
 
