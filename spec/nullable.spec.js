2bb6855e18b93df9ba1bbca31aa5922e8ac41efb
tolyo <anatolyostrovsky@gmail.com>
2017-01-18 23:14:36 +0200

    Update nullable.spec.js
    Format

--- spec/nullable.spec.js
+++ spec/nullable.spec.js
@@ -4,15 +4,15 @@ import test from 'tape';
 test('nullable constaint', (assert) => {
 
 	[, null, undefined].forEach(x =>
-  	assert.equal(nullable(false)(x), false, 'should be invalid')
-  );
+  		assert.equal(nullable(false)(x), false, 'should be invalid')
+  	);
 
 	[1, "", {}, 0].forEach(x =>
 		assert.equal(nullable(false)(x), true, 'should be valid')
 	);
 
 	[, null, undefined].forEach(x =>
-  	assert.equal(nullable(true)(x), true, 'should be valid')
+  		assert.equal(nullable(true)(x), true, 'should be valid')
 	);
 
 	assert.end();
